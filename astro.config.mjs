8eacab5f672762f6829712d4be9ea842b1c67948
Sergio villegas <sergio@cuatropuntocero.solutions>
2025-08-19 11:56:50 -0700

    Init commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,13 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      watch: {
+        // Mejora la detección de cambios en Windows/FS lentos
+        usePolling: true,
+        interval: 200,
+      },
+    },
   },
   // Add your domain here
    site: 'https://lexingtonthemes.com',
---
+++ src/data/juegos.ts
@@ -0,0 +1,563 @@
+export const juegos = [
+  {
+    nombre: "ART ROBBERY",
+    jugadores: "2-5",
+    tiempo: "20 minutos",
+    complejidad: 1.3,
+    fotos: [
+      "https://live.staticflickr.com/65535/51549747825_9a95ef0c3e_b.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/341935/art-robbery",
+  },
+  {
+    nombre: "REGICIDE",
+    jugadores: "1-4",
+    tiempo: "10-30 minutos",
+    complejidad: 2.3,
+    fotos: [
+      "https://i0.wp.com/live.staticflickr.com/65535/52631476030_5005bf81ae_b.jpg?resize=750%2C422&ssl=1",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/307002/regicide",
+  },
+  {
+    nombre: "TINDERBLOX",
+    jugadores: "2-6",
+    tiempo: "3-15 minutos",
+    complejidad: 1.0,
+    fotos: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmlr4L62pNUV46O4i0Qu4OarEFFb8_EWNxWd-ByHbqWHoutln4WROvutRI-ap5NR8eW-Y&usqp=CAU",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/295192/tinderblox",
+  },
+  {
+    nombre: "THE WHITE CASTLE",
+    jugadores: "1-4",
+    tiempo: "60-80 minutos",
+    complejidad: 3.0,
+    fotos: [
+      "https://cf.geekdo-images.com/WtiTwbdFW5bxQq_1uny7Bg__opengraph/img/P9gnZRpV8tncDNkDSrfO97-QVVQ=/fit-in/1200x630/filters:strip_icc()/pic7753808.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/371942/the-white-castle",
+  },
+  {
+    nombre: "TRIO",
+    jugadores: "3-6",
+    tiempo: "15 minutos",
+    complejidad: 1.5,
+    fotos: [
+      "https://cf.geekdo-images.com/IAsae8JH545gjeCIqSBEwg__imagepage/img/xZj40zXmv82uR4QWFuqI0q5lBXs=/fit-in/900x600/filters:no_upscale():strip_icc()/pic7754068.png",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/352515/trio",
+  },
+  {
+    nombre: "FRIEGAS TU",
+    jugadores: "2-8",
+    tiempo: "15-20 minutos",
+    complejidad: 1.2,
+    fotos: [
+      "https://kinuma.com/42839-large_default/friegas-tu-divertido-juego-de-cartas-para-2-8-jugadores.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/371544/friegas-tu",
+  },
+  {
+    nombre: "YOKAI PAGODA",
+    jugadores: "2-5",
+    tiempo: "10-15 minutos",
+    complejidad: 1.33,
+    fotos: [
+      "https://malditosdados.com/cdn/shop/files/Yokai-3.jpg?v=1748101990&width=1946",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/394127/yokai-pagoda",
+  },
+  {
+    nombre: "PELUSAS REVOLUTION",
+    jugadores: "2-5",
+    tiempo: "15 minutos",
+    complejidad: 1.2,
+    fotos: [
+      "https://consolaytablero.com/wp-content/uploads/2024/09/Pelusas-Revolution.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/339214/no-mercy",
+  },
+  {
+    nombre: "PELUSAS",
+    jugadores: "2-5",
+    tiempo: "15 minutos",
+    complejidad: 1.2,
+    fotos: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXOShpa4vTIz9uIZ-WS1jBdDmDNW4BGUTvaQ&s",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/339214/no-mercy",
+  },
+  {
+    nombre: "CITIES",
+    jugadores: "2-4",
+    tiempo: "40 minutos",
+    complejidad: 2.0,
+    fotos: [
+      "https://cf.geekdo-images.com/0SYHNQpbLeDoCUa9xfJyjA__medium/img/p8Z-Mu2N2K_4MYCRSG1wxRzLjog=/fit-in/500x500/filters:no_upscale():strip_icc()/pic8303509.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/402676/cities",
+  },
+  {
+    nombre: "FAR AWAY",
+    jugadores: "2",
+    tiempo: "90-150 minutos",
+    complejidad: 3.0,
+    fotos: [
+      "https://cf.geekdo-images.com/ekj5s_ZBIz6_AqbQMAKLgw__imagepage/img/SAjFzUtRmCCtQmb_Rpqs9z0eUDI=/fit-in/900x600/filters:no_upscale():strip_icc()/pic7731199.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/262939/far-away",
+  },
+  {
+    nombre: "WYRMSPAN",
+    jugadores: "1-5",
+    tiempo: "90-120 minutos",
+    complejidad: 2.75,
+    fotos: [
+      "https://http2.mlstatic.com/D_NQ_NP_749286-MLM77743386956_072024-O.webp",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/410201/wyrmspan",
+  },
+  {
+    nombre: "CLANK! CATACOMBS",
+    jugadores: "2-4",
+    tiempo: "45-90 minutos",
+    complejidad: 2.5,
+    fotos: [
+      "https://geekdad.com/wp-content/uploads/2022/11/clank-featured.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/365717/clank-catacombs",
+  },
+  {
+    nombre: "5-MINUTE DUNGEON",
+    jugadores: "2-5",
+    tiempo: "5 minutos",
+    complejidad: 1.5,
+    fotos: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAmgDE2DJfzLHMHiTcH7J-tTiSm7BNAxah0w&s",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/207830/5-minute-dungeon",
+  },
+
+  {
+    nombre: "SPOT IT (Dobble)",
+    jugadores: "2-8",
+    tiempo: "15 minutos",
+    complejidad: 1.06,
+    fotos: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTot6U9je7gkDSq98teiYK4n2wKUZ0ePpFADA&s",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/63268/spot-it",
+  },
+  {
+    nombre: "TACO CAT GOAT CHEESE PIZZA",
+    jugadores: "2-8",
+    tiempo: "10-15 minutos",
+    complejidad: 1.06,
+    fotos: [
+      "https://www.infiniteskill.com.mx/wp-content/uploads/2024/12/Taco-Gato-Cabra-Queso-Pizza_5.png",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/253664/taco-cat-goat-cheese-pizza",
+  },
+  {
+    nombre: "FANTASMA BLITZ",
+    jugadores: "2-8",
+    tiempo: "20 minutos",
+    complejidad: 1.14,
+    fotos: [
+      "https://cf.geekdo-images.com/0VvL-xYBsePb1DaV7AtpPw__imagepage/img/NesoO9BPQ7IbfRp7H3hC-Gztx4M=/fit-in/900x600/filters:no_upscale():strip_icc()/pic1322076.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/83195/ghost-blitz",
+  },
+  {
+    nombre: "THE CREW: MISSION DEEP SEA",
+    jugadores: "2-5",
+    tiempo: "20 minutos",
+    complejidad: 2.04,
+    fotos: [
+      "https://cf.geekdo-images.com/ryKBI_ewxSnVmmyU6DE54w__imagepage/img/W6jKOCC4PWKenCYGk_BA3GVhVRM=/fit-in/900x600/filters:no_upscale():strip_icc()/pic6471824.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/324856/the-crew-mission-deep-sea",
+  },
+  {
+    nombre: "THE BINDING OF ISAAC: FOUR SOULS",
+    jugadores: "2-4",
+    tiempo: "60-90 minutos",
+    complejidad: 2.5,
+    fotos: [
+      "https://i.kickstarter.com/assets/021/662/180/97e5c249dee6b4488b268f9b5ba454cf_original.png?anim=false&fit=cover&gravity=auto&height=873&origin=ugc&q=92&v=1529970458&width=1552&sig=rfeWYjrfsKf4FDW3LYcI4pA%2BZDJIdI2MFCbe9OGnQeE%3D",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/255984/the-binding-of-isaac-four-souls",
+  },
+  {
+    nombre: "DUNGEON MAYHEM",
+    jugadores: "2-6",
+    tiempo: "10-15 minutos",
+    complejidad: 1.5,
+    fotos: [
+      "https://thepopinsider.com/wp-content/uploads/sites/6/2020/03/WofC_DnD_MonsterMadness_featured.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/301044/dungeon-mayhem-monster-madness",
+  },
+  {
+    nombre: "DIXIT ODYSSEY",
+    jugadores: "3-8",
+    tiempo: "30 minutos",
+    complejidad: 1.2,
+    fotos: [
+      "https://smileygamesc.com/cdn/shop/files/1697732163748.jpg?v=1697732172&width=1946",
+    ],
+    enlace: "https://boardgamegeek.com/images/thing/39856",
+  },
+  {
+    nombre: "SPLENDOR",
+    jugadores: "2-4",
+    tiempo: "30 minutos",
+    complejidad: 1.78,
+    fotos: [
+      "https://cf.geekdo-images.com/sUIb5Fw0LwoEME8AIo-Wcg__imagepage/img/nyNrNeniIkDoqE7GE-zeJqMtjnA=/fit-in/900x600/filters:no_upscale():strip_icc()/pic2414323.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/148228/splendor",
+  },
+  {
+    nombre: "CAMEL UP",
+    jugadores: "3-8",
+    tiempo: "30-40 minutos",
+    complejidad: 1.2,
+    fotos: [
+      "https://cf.geekdo-images.com/qnR2V3YsW963_JaVznBjsw__itemrep/img/TrnqwwsQs1i6jrrhudXSJRBD6Gk=/fit-in/246x300/filters:strip_icc()/pic8584959.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/260605/camel-up-second-edition",
+  },
+  {
+    nombre: "L.L.A.M.A.",
+    jugadores: "2-6",
+    tiempo: "20 minutos",
+    complejidad: 1.2,
+    fotos: [
+      "https://i.ytimg.com/vi/a2MycpVJVsg/maxresdefault.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/266083/llama",
+  },
+  {
+    nombre: "CAT IN THE BOX",
+    jugadores: "2-5",
+    tiempo: "20-40 minutos",
+    complejidad: 1.2,
+    fotos: [
+      "https://cf.geekdo-images.com/eEPTtLF58WR0QKu8njUhiQ__imagepage/img/o1gzF1Tt1-Fyn2YjF9Khbdmy75M=/fit-in/900x600/filters:no_upscale():strip_icc()/pic7008548.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/345972/cat-in-the-box-deluxe-edition",
+  },
+  {
+    nombre: "CODIGO SECRETO",
+    jugadores: "4+",
+    tiempo: "15 minutos",
+    complejidad: 1.6,
+    fotos: [
+      "https://cdn.prod.website-files.com/67d17a45b4e1484b40ea8869/6836d21ca53cab16be9405f5_745014388-dsc00374_edits_sdj%20copy.webp",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/178900/codenames/images",
+  },
+  {
+    nombre: "VIRUS!",
+    jugadores: "2-6",
+    tiempo: "20-30 minutos",
+    complejidad: 1.7,
+    fotos: [
+      "https://cdn11.bigcommerce.com/s-ftsflnse4o/images/stencil/1280x1280/products/318529/1303911/D_773493-MLA72479061663_102023-O__67690.1731167358.jpg?c=1?imbypass=on",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/180020/virus",
+  },
+  {
+    nombre: "CATAN",
+    jugadores: "3-4",
+    tiempo: "60-90 minutos",
+    complejidad: 2.3,
+    fotos: [
+      "https://assets.nintendo.com/image/upload/q_auto/f_auto/ncom/software/switch/70010000068417/f75365bce6cbfbf0e1678f1ce87fe8631234b505149e22a93464120bd8f9b98a",
+    ],
+    enlace: "https://boardgamegeek.com/images/boardgame/13/catan",
+  },
+  {
+    nombre: "DRAFTOSAURUS",
+    jugadores: "2-5",
+    tiempo: "15 minutos",
+    complejidad: 1.22,
+    fotos: [
+      "https://www.infiniteskill.com.mx/wp-content/uploads/2023/04/Draftosaurus_3.png",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/264055/draftosaurus",
+  },
+  {
+    nombre: "SEA SALT & PAPER",
+    jugadores: "2-4",
+    tiempo: "30 minutos",
+    complejidad: 2.1,
+    fotos: [
+      "https://b1803394.smushcdn.com/1803394/wp-content/uploads/2022/11/Sea-Salt-Header-990x557.jpg?lossy=1&strip=1&webp=1",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/367220/sea-salt-and-paper",
+  },
+  {
+    nombre: "INCOMODOS INVITADOS",
+    jugadores: "2-8",
+    tiempo: "60 minutos",
+    complejidad: 2.8,
+    fotos: [
+      "https://http2.mlstatic.com/D_NQ_NP_708523-MLM26652857496_012018-O.webp",
+    ],
+    // El prompt da enlace para "AWKWARD GUESTS", que es el nombre en inglés.
+    enlace: "https://boardgamegeek.com/boardgame/188866/awkward-guests-the-walton-case",
+  },
+  {
+    nombre: "7 WONDERS: ARCHITECTS",
+    jugadores: "2-7",
+    tiempo: "25 minutos",
+    complejidad: 1.40,
+    fotos: [
+      "https://cdn.svc.asmodee.net/production-rprod/storage/games/7-wonders-architects/Box/7arc-3dbox-left-16303345034Kwxc-large.png"],
+    enlace: "https://boardgamegeek.com/boardgame/346703/7-wonders-architects",
+  },
+  {
+    nombre: "LONG SHOT",
+    jugadores: "1-8",
+    tiempo: "25 minutos",
+    complejidad: 1.93,
+    fotos: [
+      "https://cf.geekdo-images.com/GeOiFVmyTRi7oW1VjsSxGg__imagepage/img/kPWH8RgWHayqQmPp3Ng4_M5hbS8=/fit-in/900x600/filters:no_upscale():strip_icc()/pic6746326.jpg"],
+    enlace: "https://boardgamegeek.com/boardgame/295374/long-shot-the-dice-game",
+  },
+  {
+    nombre: "CITADELS",
+    jugadores: "2-8",
+    tiempo: "30-60 minutos",
+    complejidad: 2.4,
+    fotos: [
+      "https://cf.geekdo-images.com/42iW4E-vOumFXRQAiSf-GA__itemrep/img/NVAwUCUVc6QWNKZNUBiIMg2nk8A=/fit-in/246x300/filters:strip_icc()/pic3239104.jpg",
+    ],
+    enlace: null,
+  },
+  {
+    nombre: "THE MIND",
+    jugadores: "2-4",
+    tiempo: "15-20 minutos",
+    complejidad: 1.1,
+    fotos: [
+      "https://cf.geekdo-images.com/X8YcgoDrwYfJztrWLCUMkg__imagepage/img/sABT82V_fm0eSRg08EznI96wbAk=/fit-in/900x600/filters:no_upscale():strip_icc()/pic4062083.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/244992/the-mind",
+  },
+  {
+    nombre: "TEN",
+    jugadores: "2-5",
+    tiempo: "20-30 minutos",
+    complejidad: 2.1,
+    fotos: [
+      "https://www.thefandomentals.com/wp-content/uploads/2021/10/IMG-5058-768x576.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/335609/ten",
+  },
+  {
+    nombre: "CUBIRDS",
+    jugadores: "2-5",
+    tiempo: "20 minutos",
+    complejidad: 1.7,
+    fotos: [
+      "https://boardgamecircus.com/wp-content/uploads/2019/07/cubirds-teaser-1200px-1030x1030.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/245476/cubirds",
+  },
+  {
+    nombre: "COUP",
+    jugadores: "2-6",
+    tiempo: "10-15 minutos",
+    complejidad: 1.4,
+    fotos: [
+      "https://dailyevergreen.com/wp-content/uploads/2023/01/9D3748CF-7F0C-4DE8-8F5B-67CF9D61CBEF.jpeg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/131357/coup",
+  },
+  {
+    nombre: "POISONS",
+    jugadores: "3-8",
+    tiempo: "15 minutos",
+    complejidad: 1,
+    fotos: [
+      "https://i0.wp.com/www.thefamilygamers.com/wp-content/uploads/Poisons_cover.jpg?resize=860%2C584&ssl=1",
+    ],
+    // No hay enlace en el prompt para "POISONS"
+    enlace: "https://boardgamegeek.com/boardgame/286236/poisons",
+  },
+  {
+    nombre: "SECRET HITLER",
+    jugadores: "5-10",
+    tiempo: "45 minutos",
+    complejidad: 1.74,
+    fotos: [
+      "https://cf.geekdo-images.com/AdhFw-OgOBv2JiQuU59wag__imagepage/img/uHR2MjQj5QDcoJ6ScoWISHX8-2A=/fit-in/900x600/filters:no_upscale():strip_icc()/pic3136390.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/188834/secret-hitler",
+  },
+  {
+    nombre: "DRAGONWOOD",
+    jugadores: "2-4",
+    tiempo: "20-30 minutos",
+    complejidad: 2.0,
+    fotos: [
+      "https://thesoloboardgamer.com/wp-content/uploads/dragonwood-2.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/172060/dragonwood",
+  },
+  {
+    nombre: "BLOCKBUSTER",
+    jugadores: "4+",
+    tiempo: "30-35 minutos",
+    complejidad: 2.0,
+    fotos: [
+      "https://criticalpopcorn.com/wp-content/uploads/2019/12/blockbuster-party-game_36307.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/257142/blockbuster-the-game",
+  },
+  {
+    nombre: "NOVA LUNA",
+    jugadores: "1-4",
+    tiempo: "30-60 minutos",
+    complejidad: 3.0,
+    fotos: [
+      "https://cf.geekdo-images.com/W_a3K6tUdXSLG93wb612Yg__opengraph/img/xM-ZTCAyDr4gxvqiUDeS5y-4GYE=/fit-in/1200x630/filters:strip_icc()/pic5813002.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/284471/nova-luna",
+  },
+  {
+    nombre: "SUSHI GO!",
+    jugadores: "2-5",
+    tiempo: "15 minutos",
+    complejidad: 1.5,
+    fotos: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcA3YVHUIouF58NKPNqyCCdJWlmMlx4xY-ew&s",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/133473/sushi-go",
+  },
+  {
+    nombre: "ROOT",
+    jugadores: "2-4",
+    tiempo: "60-90 minutos",
+    complejidad: 4.0,
+    fotos: [
+      "https://live.staticflickr.com/65535/48442446297_f7ffd89ca2_b.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/237182/root",
+  },
+  {
+    nombre: "EXPLODING KITTENS",
+    jugadores: "2-10",
+    tiempo: "15 minutos",
+    complejidad: 1.5,
+    fotos: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs2836p1c4asZCBWC2w2MLKyf41C1kzIi1BQ&s",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/172225/exploding-kittens",
+  },
+  {
+    nombre: "FANTASY REALMS",
+    jugadores: "2-6",
+    tiempo: "20 minutos",
+    complejidad: 1.82,
+    fotos: [
+      "https://www.eurojuegos.com.mx/wp-content/uploads/2021/10/Fantasy-Realms-Caja.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/223040/fantasy-realms",
+  },
+  {
+    nombre: "OHANAMI",
+    jugadores: "2-4",
+    tiempo: "20 minutos",
+    complejidad: 2.0,
+    fotos: [
+      "https://whatsericplaying.com/wp-content/uploads/2020/12/gameplay-2-5.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/270314/ohanami",
+  },
+  {
+    nombre: "NO THANKS!",
+    jugadores: "3-7",
+    tiempo: "20 minutos",
+    complejidad: 1.5,
+    fotos: [
+      "https://preview.redd.it/too-many-card-games-heres-a-storage-solution-for-flip7-v0-n9vhd8equ6ae1.jpg?width=640&crop=smart&auto=webp&s=50831049bcdff4a8562830474fa4bb2244b07105",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/12942/no-thanks",
+  },
+  {
+    nombre: "ONE DECK DUNGEON",
+    jugadores: "1-2",
+    tiempo: "30-45 minutos",
+    complejidad: 2.34,
+    fotos: [
+      "https://cdn11.bigcommerce.com/s-7eepw5u9z2/images/stencil/1280x1280/products/2737/3267/6143a95710-6-131__90494.1734712833.jpg?c=1",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/179275/one-deck-dungeon",
+  },
+  {
+    nombre: "TARGI",
+    jugadores: "2",
+    tiempo: "45-60 minutos",
+    complejidad: 2.5,
+    fotos: [
+      "https://m.media-amazon.com/images/I/81emL15oewL.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/118048/targi",
+  },
+  {
+    nombre: "MINDBUG",
+    jugadores: "2",
+    tiempo: "15-20 minutos",
+    complejidad: 2.5,
+    fotos: [
+      "https://cf.geekdo-images.com/1zn-crolRPg-JKbkp89PHg__imagepage/img/mNrIb6nGrZKblDWlpuw4DLhePhM=/fit-in/900x600/filters:no_upscale():strip_icc()/pic6541018.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/345584/mindbug-first-contact",
+  },
+  {
+    nombre: "THE RESISTANCE: AVALON",
+    jugadores: "5-10",
+    tiempo: "30 minutos",
+    complejidad: 2.4,
+    fotos: [
+      "https://cf.geekdo-images.com/imagepage/img/DOVoaCcHQOdFvLl93yWTowK5tts=/fit-in/900x600/filters:no_upscale()/pic4927914.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/128882/the-resistance-avalon",
+  },
+  {
+    nombre: "UNSTABLE UNICORNS",
+    jugadores: "2-8",
+    tiempo: "30-60 minutos",
+    complejidad: 2.5,
+    fotos: [
+      "https://unstablegames.com/cdn/shop/files/UU-NSFW-2E-Base-Game_d7ac5607-34e6-455f-a4c7-b27803afa4b6_grande.png?v=1736555954",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/234190/unstable-unicorns",
+  },
+  {
+    nombre: "MONOPOLY",
+    jugadores: "2-8",
+    tiempo: "60-90 minutos",
+    complejidad: 1.3,
+    fotos: [
+      "https://http2.mlstatic.com/D_NQ_NP_990550-MLM31780337440_082019-O-juegos-de-mesa-monopoly-normal-clasico-hasbro-original-token.webp",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/1406/monopoly",
+  },
+  {
+    nombre: "MASCARADE",
+    jugadores: "2-13",
+    tiempo: "30 minutos",
+    complejidad: 2.3,
+    fotos: [
+      "https://cf.geekdo-images.com/RF-r6_NU8y6QdJHCDt3qJQ__imagepage/img/MHS-KDDuxqxyUzTdmEkCg8BjUCs=/fit-in/900x600/filters:no_upscale():strip_icc()/pic1762406.jpg",
+    ],
+    enlace: "https://boardgamegeek.com/boardgame/139030/mascarade",
+  },
+];
